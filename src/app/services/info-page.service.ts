9b4f9de3ede1298f7a201840af55b8a5fdbca02a
Carlos Cantalapiedra <carloscantalapiedrathomas@gmail.com>
2020-07-01 18:14:39 +0200

    Template with files in Firebase

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -9,17 +9,30 @@ export class InfoPageService {
 
   info: InfoPage = {};
   loaded = false;
+  
+  equipo: any[] = []
 
   constructor( private http: HttpClient) {
     console.log('InfoPage Service');
 
+    this.loadInfo();
+    this.loadEquipo();
+
+
+  }
+  private loadInfo() {
     this.http.get('assets/data/data-page.json')
-      .subscribe((resp: InfoPage) => {
-        this.loaded = true;
-        this.info = resp;
-        console.log(resp);
-      });
+    .subscribe((resp: InfoPage) => {
+      this.loaded = true;
+      this.info = resp;
+    });
+  }
 
-
+  private loadEquipo() {
+    this.http.get('https://angular-html-6c575.firebaseio.com/equipo.json')
+    .subscribe( (resp: any []) => {
+      this.equipo = resp;
+      console.log(resp);
+    });
   }
 }
